f5b1ab91c6b5b300f1c542678b7cefb5dd96104b
Didier Defrance <d2f.defrance@gmail.com>
2019-01-10 11:47:23 +0100

    dao simu

---
+++ src/main/java/com/m2i/tp/dao/DaoCompteJpa.java
@@ -0,0 +1,39 @@
+package com.m2i.tp.dao;
+
+import java.util.List;
+
+import com.m2i.tp.entity.Compte;
+
+public class DaoCompteJpa implements DaoCompte {
+
+	@Override
+	public Compte findCompteByNumero(Long numero) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Compte> findAllComptes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateCompte(Compte cpt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createCompte(Compte cpt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteCompte(Long numero) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
