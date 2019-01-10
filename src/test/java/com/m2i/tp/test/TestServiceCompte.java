b9da476b0a21bfc3058c4347901b6f4d68082183
Didier Defrance <d2f.defrance@gmail.com>
2019-01-10 16:26:37 +0100

    debut junit

---
+++ src/test/java/com/m2i/tp/test/TestServiceCompte.java
@@ -0,0 +1,23 @@
+package com.m2i.tp.test;
+
+import com.m2i.tp.service.ServiceCompte;
+
+//Classe de test unitaire (framework JUnit 4)
+//nécessite junit 4 dans le pom.xml
+public class TestServiceCompte {
+	
+	private ServiceCompte serviceCompte ; //à tester
+	
+	public void preparerServiceCompte() {
+		
+	}
+	
+	public void testAjoutCompte() {
+		
+	}
+	
+    public void test2() {
+		
+	}
+
+}
