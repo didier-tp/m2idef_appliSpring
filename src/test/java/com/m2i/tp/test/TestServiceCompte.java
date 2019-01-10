e8657cc2567bd640c27dc8e423f6e699db1c5967
Didier Defrance <d2f.defrance@gmail.com>
2019-01-10 16:34:44 +0100

    a

--- src/test/java/com/m2i/tp/test/TestServiceCompte.java
+++ src/test/java/com/m2i/tp/test/TestServiceCompte.java
@@ -1,23 +1,41 @@
 package com.m2i.tp.test;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.m2i.tp.entity.Compte;
+//org.junit = Junit4
 import com.m2i.tp.service.ServiceCompte;
 
 //Classe de test unitaire (framework JUnit 4)
 //nécessite junit 4 dans le pom.xml
 public class TestServiceCompte {
 	
-	private ServiceCompte serviceCompte ; //à tester
+	private ServiceCompte serviceCompte=null ; //à tester
 	
+	@Before //méthode (re-)lancée avec chaque @Test
 	public void preparerServiceCompte() {
-		
+		ApplicationContext contextSpring =
+				new ClassPathXmlApplicationContext("mySpringConf.xml");
+		this.serviceCompte = (ServiceCompte) contextSpring.getBean("serviceCompteImpl");
+		               //ou bien contextSpring.getBean(ServiceCompte.class);
 	}
 	
+	
+	@Test
 	public void testAjoutCompte() {
-		
+		Compte cpt = new Compte(null,"compte 1",100.0);
+		this.serviceCompte.saveOrUpdateCompte(cpt);
+		Assert.assertTrue(cpt.getNumero()!=null);
+		System.out.println("cpt="+cpt.toString());
 	}
 	
+	@Test
     public void test2() {
-		
+		Assert.assertTrue(1+1==2);
 	}
 
 }
