382ab790aa533e5820012424e04a2a72d2e89271
Didier Defrance <d2f.defrance@gmail.com>
2019-01-11 09:16:52 +0100

    avec spring-test

--- src/test/java/com/m2i/tp/test/TestServiceCompte.java
+++ src/test/java/com/m2i/tp/test/TestServiceCompte.java
@@ -1,39 +1,23 @@
 package com.m2i.tp.test;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.m2i.tp.entity.Compte;
 //org.junit = Junit4
 import com.m2i.tp.service.ServiceCompte;
 
-//Classe de test unitaire (framework JUnit 4)
-//nécessite junit 4 dans le pom.xml
+//nécessite spring-test dans pom.xml
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/mySpringConf.xml")
 public class TestServiceCompte {
-	private ServiceCompte serviceCompte=null ; //à tester
-	private static ApplicationContext contextSpring =null;
 	
-	@BeforeClass //méthode (re-)lancée avec chaque @Test
-	public static void preparerChoseStatic() {
-		contextSpring =
-				new ClassPathXmlApplicationContext("mySpringConf.xml");
-	}
-	
-	@AfterClass
-	public static void finaliserChoseStatic() {
-		((ClassPathXmlApplicationContext)contextSpring).close();
-	}
-	
-	@Before //méthode (re-)lancée avec chaque @Test
-	public void preparerServiceCompte() { 
-		this.serviceCompte = (ServiceCompte) contextSpring.getBean("serviceCompteImpl");
-		               //ou bien contextSpring.getBean(ServiceCompte.class);
-	}
+	@Autowired
+	private ServiceCompte serviceCompte ; //à tester
 	
 	
 	@Test
